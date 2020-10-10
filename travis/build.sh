c67985a7cdbcce015763502cc5c14feed70927b9
Andrea Cavalli <andrea@cavallium.it>
2020-10-10 19:21:08 +0200

    Update .travis.yml and build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -6,13 +6,19 @@ fi
 
 source ./travis/setup_variables.sh
 
-cd ./scripts/
+SCRIPTS_DIR=$(realpath ./scripts/)
 
+cd ${SCRIPTS_DIR}
 ./generate_maven_project.sh
+cd ${SCRIPTS_DIR}
 ./generate_td_tools.sh
+cd ${SCRIPTS_DIR}
 ./compile_td.sh
+cd ${SCRIPTS_DIR}
 ./generate_tdapi_java_file.sh
+cd ${SCRIPTS_DIR}
 ./compile_tdjni.sh
+cd ${SCRIPTS_DIR}
 ./build_generated_maven_project.sh
 
 echo "Done."
