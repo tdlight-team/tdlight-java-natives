d383396097030a221def2f4cd5c9de1f19965091
Andrea Cavalli <andrea@cavallium.it>
2020-12-26 13:47:45 +0100

    Update setup-variables.sh

--- github-workflows/setup-variables.sh
+++ github-workflows/setup-variables.sh
@@ -5,7 +5,7 @@ export MAVEN_OPTS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.
 echo "MATRIX_OS: $GH_MATRIX_OS"
 echo "MATRIX_ARCH: $GH_MATRIX_ARCH"
 
-if [ $GH_MATRIX_OS == "macos-10.15" ]; then
+if [ "$GH_MATRIX_OS" == "macos-10.15" ]; then
 	export PCRE="E"
 else
 	export PCRE="r"
@@ -14,7 +14,7 @@ fi
 export GH_CPU_ARCH=$(echo $GH_MATRIX_ARCH | sed -$PCRE 's/^(linux\/|windows\/|macos\/)?(386|amd64|arm64|ppc64le|arm)(\/(v6|v7))?$/\2\4/g')
 export GH_OS_NAME=$(echo $GH_MATRIX_ARCH | sed -$PCRE 's/^((linux)\/|(windows)\/|(macos)\/)?(386|amd64|arm64|ppc64le|arm)(\/(v6|v7))?$/\2\3\4/g')
 
-if [[ -z "GH_OS_NAME" ]]; then
+if [[ -z "$GH_OS_NAME" ]]; then
 	export GH_OS_NAME=$(echo $GH_MATRIX_OS | sed -$PCRE 's/^([^-]+)/\1/g')
 fi
 
