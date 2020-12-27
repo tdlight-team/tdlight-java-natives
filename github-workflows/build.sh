e5c00abbb758eb828bab3d25adba313b66077196
Andrea Cavalli <andrea@cavallium.it>
2020-12-27 14:18:13 +0100

    Update build.sh and run-all.sh

--- github-workflows/build.sh
+++ github-workflows/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 set -e
 cd ./scripts/
-source ./generate_maven_project.sh
-source ./generate_td_tools.sh
-source ./compile_td.sh
-source ./compile_tdjni.sh
-source ./build_generated_maven_project.sh
+./generate_maven_project.sh
+./generate_td_tools.sh
+./compile_td.sh
+./compile_tdjni.sh
+./build_generated_maven_project.sh
 
 echo "Done."
 exit 0
