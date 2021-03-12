e13f2ef7703c14f291620b1d88b90af3e664e994
Andrea Cavalli <andrea@cavallium.it>
2021-03-12 14:57:56 +0100

    Update tdlight

--- scripts/core/build_generated_maven_project.sh
+++ scripts/core/build_generated_maven_project.sh
@@ -2,7 +2,7 @@
 cd ../../generated
 
 [ -f ./src/main/java/it/tdlight/jni/TdApi.java ] && rm ./src/main/java/it/tdlight/jni/TdApi.java
-mvn clean package
+mvn -B -q clean package
 
 echo "Done."
 exit 0
