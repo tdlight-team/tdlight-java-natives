150dfd1791bb95a253b96ae9848522de94f80ba9
Andrea Cavalli <andrea@cavallium.it>
2021-09-26 01:23:09 +0200

    Install crossbuild-essential after build-essential

--- scripts/continuous-integration/docker/build-natives.sh
+++ scripts/continuous-integration/docker/build-natives.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
-echo "\$PWD=${PWD}"
 export JAVA_HOME
 JAVA_HOME="/usr/lib/jvm/$(find "/usr/lib/jvm/" -maxdepth 1 -type d -iname "java*jdk*" | head -n 1)/"
 export JAVA_INCLUDE_PATH="$JAVA_HOME/include"
@@ -9,6 +8,6 @@ if [ -z "${CPU_ARCH_DPKG}" ]; then
 	echo "Missing parameter: CPU_ARCH_DPKG"
 	exit 1
 fi
-sudo apt install -y "crossbuild-essential-${CPU_ARCH_DPKG}"
 source ./scripts/continuous-integration/github-workflows/install-dependencies.sh
+sudo apt install -y "crossbuild-essential-${CPU_ARCH_DPKG}"
 source ./scripts/continuous-integration/github-workflows/build-natives.sh
