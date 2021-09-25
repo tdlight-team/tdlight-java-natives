03178a15a7587aeb4f71b040e55667ad09c98cab
Andrea Cavalli <andrea@cavallium.it>
2021-09-26 01:39:26 +0200

    Wrong java home path

--- scripts/continuous-integration/docker/build-natives.sh
+++ scripts/continuous-integration/docker/build-natives.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 export JAVA_HOME
-JAVA_HOME="/usr/lib/jvm/$(find "/usr/lib/jvm/" -maxdepth 1 -type d -iname "java*jdk*" | head -n 1)/"
+JAVA_HOME="$(find "/usr/lib/jvm/" -maxdepth 1 -type d -iname "java*jdk*" | head -n 1)/"
 export JAVA_INCLUDE_PATH="$JAVA_HOME/include"
 source ./scripts/continuous-integration/github-workflows/setup-variables.sh
 # Check variables correctness
