757fdfe19b4cf21e7babb3f00606f3287c334b6c
Andrea Cavalli <andrea@cavallium.it>
2021-12-08 00:17:22 +0100

    Bugfixes

--- scripts/core/deploy_release.sh
+++ scripts/core/deploy_release.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 # OTHER REQUIRED ENVIRONMENT VARIABLES:
 #   REVISION = <revision>
+# OTHER ENVIRONMENT VARIABLES:
+#   API_TYPE = <legacy|sealed>
 
 # Check variables correctness
 if [ -z "${REVISION}" ]; then
@@ -9,10 +11,11 @@ if [ -z "${REVISION}" ]; then
 fi
 
 source ./setup-variables.sh
+FALLBACK_API_TYPE="sealed"
 
 cd ../../generated
 
-mvn -B -DrevisionNumber="${REVISION}" -DrevisionSuffix="" clean deploy
+mvn -B -DrevisionNumber="${REVISION}" "-D${API_TYPE:-${FALLBACK_API_TYPE}}" -DrevisionSuffix="" clean deploy
 
 echo "Done."
 exit 0
--- scripts/core/deploy_snapshot.sh
+++ scripts/core/deploy_snapshot.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -e
+# OTHER ENVIRONMENT VARIABLES:
+#   API_TYPE = <legacy|sealed>
+
 cd ../../generated
 
-mvn -B deploy
+FALLBACK_API_TYPE="sealed"
+
+mvn -B "-D${API_TYPE:-${FALLBACK_API_TYPE}}" deploy
 
 echo "Done."
 exit 0
