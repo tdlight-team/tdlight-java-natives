8c5821aa490ba41a7b7d7265ae1e8255418afbb9
Andrea Cavalli <andrea@cavallium.it>
2022-02-19 00:57:06 +0100

    Bugfix

--- scripts/continuous-integration/docker/build-natives.sh
+++ scripts/continuous-integration/docker/build-natives.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
+export JAVA_HOME
+JAVA_HOME="$(find "/usr/lib/jvm/" -maxdepth 1 -type d -iname "java*jdk*" | head -n 1)/"
+export JAVA_INCLUDE_PATH="$JAVA_HOME/include"
 source ./scripts/continuous-integration/github-workflows/setup-variables.sh
 # Check variables correctness
 if [ -z "${CPU_ARCH_DPKG}" ]; then
