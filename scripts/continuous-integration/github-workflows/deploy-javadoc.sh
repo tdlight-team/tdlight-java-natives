cfa7ad35e04b3d0056320ab629af639f1f9c025e
Andrea Cavalli <andrea@cavallium.it>
2022-12-15 17:31:28 +0100

    Fix docs deployment

--- scripts/continuous-integration/github-workflows/deploy-javadoc.sh
+++ scripts/continuous-integration/github-workflows/deploy-javadoc.sh
@@ -15,7 +15,7 @@ cd "./generated-${API_TYPE}/target/apidocs"
 find . -name '*.html' -exec sed -i -r 's/<\/title>/<\/title>\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><style>\n\t#memberSummary_tabpanel{overflow-x: auto;}\n\tli.blockList{overflow-x: auto;}\n\ttd.colLast div{max-width:30vw;}\n\t#search{width: 400px;max-width: 65vw;}\n\t.title,.subTitle,pre,.inheritance,h1,h2,h3,h4,h5,.memberSummary,.memberSignature,.typeSummary,.blockList,.contentContainer{white-space:normal;word-break:break-word;}\n\tul{padding-left:10px}\n<\/style>/' {} \;
 git init
 git checkout -b "${IMPLEMENTATION_NAME}-${API_TYPE}"
-git remote add origin "ssh://git@ssh.git.ignuranza.net/tdlight-team/tdlight-docs"
+git remote add origin "https://${DOCS_DEPLOY_USERNAME}:${DOCS_DEPLOY_PASSWORD}@git.ignuranza.net/tdlight-team/tdlight-docs"
 git config user.email "andrea@warp.ovh"
 git config user.name "Andrea Cavalli"
 git fetch --all
