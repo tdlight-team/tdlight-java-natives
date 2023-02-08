f3d98c0cbca6db23991b37f66008f047054cad7a
Andrea Cavalli <andrea@warp.ovh>
2023-02-08 17:32:48 +0100

    Update github actions to Ubuntu 22.04 and OpenSSL3

--- scripts/continuous-integration/docker/install-dependencies.sh
+++ scripts/continuous-integration/docker/install-dependencies.sh
@@ -4,8 +4,12 @@ if [ -z "${CPU_ARCH_DPKG}" ]; then
 	echo "Missing parameter: CPU_ARCH_DPKG"
 	exit 1
 fi
+OJDK_VERSION_NUMBER="11"
+if [[ "$GH_MATRIX_SSL" == "ssl3" ]]; then
+  OJDK_VERSION_NUMBER="17"
+fi
 install_deps() {
-  apt-get install -y sudo build-essential openjdk-11-jdk locales wget cmake gperf ccache maven \
+  apt-get install -y sudo build-essential openjdk-${OJDK_VERSION_NUMBER}-jdk locales wget cmake gperf ccache maven \
           libssl-dev ${SSL_LIB_NAME} zlib1g-dev zlib1g "crossbuild-essential-${CPU_ARCH_DPKG}"
   sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
       dpkg-reconfigure --frontend=noninteractive locales && \
@@ -68,23 +72,23 @@ if [ -n "${CROSS_BUILD_DEPS_DIR}" ]; then
 		dpkg -x "$JC_DEB" "$CROSS_BUILD_DEPS_DIR"
 		rm "$JC_DEB"
 		# OpenJDK-JRE-Headless
-		apt-get download "openjdk-11-jre-headless:${CPU_ARCH_DPKG}"
-		OJDKRH_DEB=$(find . -name "openjdk-11-jre-headless_*.deb")
+		apt-get download "openjdk-${OJDK_VERSION_NUMBER}-jre-headless:${CPU_ARCH_DPKG}"
+		OJDKRH_DEB=$(find . -name "openjdk-${OJDK_VERSION_NUMBER}-jre-headless_*.deb")
 		dpkg -x "$OJDKRH_DEB" "$CROSS_BUILD_DEPS_DIR"
 		rm "$OJDKRH_DEB"
 		# OpenJDK-JRE
-		apt-get download "openjdk-11-jre:${CPU_ARCH_DPKG}"
-		OJDKR_DEB=$(find . -name "openjdk-11-jre_*.deb")
+		apt-get download "openjdk-${OJDK_VERSION_NUMBER}-jre:${CPU_ARCH_DPKG}"
+		OJDKR_DEB=$(find . -name "openjdk-${OJDK_VERSION_NUMBER}-jre_*.deb")
 		dpkg -x "$OJDKR_DEB" "$CROSS_BUILD_DEPS_DIR"
 		rm "$OJDKR_DEB"
 		# OpenJDK-JDK
-		apt-get download "openjdk-11-jdk-headless:${CPU_ARCH_DPKG}"
-		OJDKJ_DEB=$(find . -name "openjdk-11-jdk-headless_*.deb")
+		apt-get download "openjdk-${OJDK_VERSION_NUMBER}-jdk-headless:${CPU_ARCH_DPKG}"
+		OJDKJ_DEB=$(find . -name "openjdk-${OJDK_VERSION_NUMBER}-jdk-headless_*.deb")
 		dpkg -x "$OJDKJ_DEB" "$CROSS_BUILD_DEPS_DIR"
 		rm "$OJDKJ_DEB"
 		# OpenJDK-GUI
-		apt-get download "openjdk-11-jdk:${CPU_ARCH_DPKG}"
-		OJDKG_DEB=$(find . -name "openjdk-11-jdk_*.deb")
+		apt-get download "openjdk-${OJDK_VERSION_NUMBER}-jdk:${CPU_ARCH_DPKG}"
+		OJDKG_DEB=$(find . -name "openjdk-${OJDK_VERSION_NUMBER}-jdk_*.deb")
 		dpkg -x "$OJDKG_DEB" "$CROSS_BUILD_DEPS_DIR"
 		rm "$OJDKG_DEB"
 
