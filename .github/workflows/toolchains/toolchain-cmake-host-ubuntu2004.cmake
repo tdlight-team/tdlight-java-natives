ed2b68ec45d91e26bc1f2d662b47430c5d33f6a7
Andrea Cavalli <andrea@cavallium.it>
2023-05-07 16:39:12 +0200

    Fix toolchain

--- .github/workflows/toolchains/toolchain-cmake-host-ubuntu2004.cmake
+++ .github/workflows/toolchains/toolchain-cmake-host-ubuntu2004.cmake
@@ -2,7 +2,7 @@ SET(CMAKE_SYSTEM_NAME Linux)
 
 set(CMAKE_C_COMPILER clang-10)
 set(CMAKE_CXX_COMPILER clang++-10)
-set(CMAKE_AR llvm-ar)
+set(CMAKE_AR llvm-ar-10)
 
 # Set various compiler flags
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-psabi --specs=nosys.specs -fdata-sections -ffunction-sections -Wl,--gc-sections")
@@ -10,5 +10,4 @@ set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} ${CMAKE_CXX_FLAGS} -stdlib=libc++ -fno-exc
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++ -lc++ -lc++abi")
 
 # Use LLD on linux
-#add_link_options("-fuse-ld=lld" "-rtlib=compiler-rt" "-unwindlib=libunwind" "-Wl,--as-needed")
-add_link_options("-fuse-ld=lld" "-v")
+add_link_options("-fuse-ld=lld")
--- .github/workflows/toolchains/toolchain-cmake-host-ubuntu2204.cmake
+++ .github/workflows/toolchains/toolchain-cmake-host-ubuntu2204.cmake
@@ -2,7 +2,7 @@ SET(CMAKE_SYSTEM_NAME Linux)
 
 set(CMAKE_C_COMPILER clang-14)
 set(CMAKE_CXX_COMPILER clang++-14)
-set(CMAKE_AR llvm-ar)
+set(CMAKE_AR llvm-ar-14)
 
 # Set various compiler flags
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-psabi --specs=nosys.specs -fdata-sections -ffunction-sections -Wl,--gc-sections")
@@ -11,4 +11,4 @@ set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++ -lc++ -lc++
 
 # Use LLD on linux
 #add_link_options("-fuse-ld=lld" "-rtlib=compiler-rt" "-unwindlib=libunwind" "-Wl,--as-needed")
-add_link_options("-fuse-ld=lld" "-v")
+add_link_options("-fuse-ld=lld")
