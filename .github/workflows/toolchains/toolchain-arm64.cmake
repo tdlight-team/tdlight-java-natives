bae610df91b912b1813b5e7e62f310f5dddfc0bd
Andrea Cavalli <andrea@cavallium.it>
2023-05-07 19:15:20 +0200

    fix

--- .github/workflows/toolchains/toolchain-arm64.cmake
+++ .github/workflows/toolchains/toolchain-arm64.cmake
@@ -50,10 +50,3 @@ set(THREADS_PREFER_PTHREAD_FLAG ON)
 # Use LLD on linux
 #add_link_options("-fuse-ld=lld" "-rtlib=compiler-rt" "-unwindlib=libunwind" "-Wl,--as-needed")
 add_link_options("-fuse-ld=lld")
-
-# ccache
-find_program(CCACHE_FOUND ccache)
-if(CCACHE_FOUND)
-    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
-    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache) # Less useful to do it for linking, see edit2
-endif(CCACHE_FOUND)
