338a85da4deb245332f6da0978a177c6ff467fdf
Andrea Cavalli <andrea@cavallium.it>
2023-05-08 15:09:34 +0200

    fix deps

--- .github/workflows/scripts/install-deps-linux-arm64-ssl1.sh
+++ .github/workflows/scripts/install-deps-linux-arm64-ssl1.sh
@@ -12,9 +12,12 @@ echo "deb [arch=arm64] http://ports.ubuntu.com/ $(lsb_release -cs)-security main
 apt-get --assume-yes update
 
 # Install and bypass a bug in the cross-platform libc++ packages
-apt-get --assume-yes autoremove libc++-10-dev libc++abi-10-dev || true
+apt-get --assume-yes autoremove libc++-10-dev libc++abi-10-dev libc++1-10 libc++abi1-10 || true
 apt-get --assume-yes -o Dpkg::Options::="--force-overwrite" install libc++-10-dev:arm64 libc++abi-10-dev:arm64
-cp -r --remove-destination /usr/lib/llvm-10/lib/. /usr/lib/aarch64-linux-gnu/
+rm -r /usr/lib/aarch64-linux-gnu/libc++.so.1.0 /usr/lib/aarch64-linux-gnu/libc++abi.so.1.0 /usr/lib/aarch64-linux-gnu/libc++abi.so
+mv /usr/lib/llvm-10/lib/libc++.so.1.0 /usr/lib/aarch64-linux-gnu/
+mv /usr/lib/llvm-10/lib/libc++abi.so.1.0 /usr/lib/aarch64-linux-gnu/
+mv /usr/lib/llvm-10/lib/libc++abi.so /usr/lib/aarch64-linux-gnu/
 apt-get --assume-yes -o Dpkg::Options::="--force-overwrite" install libc++-10-dev libc++abi-10-dev
 # End libc++ packages bugfix
 
