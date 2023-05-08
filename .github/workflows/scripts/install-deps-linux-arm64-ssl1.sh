52bf29d34b5527286094e21b1e4cc43b0d27e892
Andrea Cavalli <andrea@cavallium.it>
2023-05-08 14:33:32 +0200

    Enable ci

--- .github/workflows/scripts/install-deps-linux-arm64-ssl1.sh
+++ .github/workflows/scripts/install-deps-linux-arm64-ssl1.sh
@@ -10,5 +10,15 @@ echo "deb [arch=arm64] http://ports.ubuntu.com/ $(lsb_release -cs)-updates main 
 echo "deb [arch=arm64] http://ports.ubuntu.com/ $(lsb_release -cs)-security main universe restricted multiverse" >> /etc/apt/sources.list
 
 apt-get --assume-yes update
-apt-get --assume-yes install libssl-dev:arm64 zlib1g-dev:arm64 openjdk-8-jdk-headless:arm64
 
+# Install and bypass a bug in the cross-platform libc++ packages
+apt-get --assume-yes autoremove libc++-10-dev libc++abi-10-dev
+apt-get --assume-yes install libc++-10-dev:arm64 libc++abi-10-dev:arm64
+cp --remove-destination /usr/lib/llvm-10/lib/libc++.so.1.0 /usr/lib/aarch64-linux-gnu/
+cp --remove-destination /usr/lib/llvm-10/lib/libc++abi.so.1.0 /usr/lib/aarch64-linux-gnu/
+cp --remove-destination /usr/lib/llvm-10/lib/libc++abi.so /usr/lib/aarch64-linux-gnu/
+apt-get --assume-yes -o Dpkg::Options::="--force-overwrite" install libc++-10-dev libc++abi-10-dev
+# End libc++ packages bugfix
+
+# Install dependencies
+apt-get --assume-yes install libssl-dev:arm64 zlib1g-dev:arm64 openjdk-8-jdk-headless:arm64 libgcc-9-dev:arm64 libc6-dev:arm64
