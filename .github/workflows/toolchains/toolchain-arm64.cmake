f98ca55ac55697d3a43c41bb5b798534a8eb73f0
Andrea Cavalli <andrea@cavallium.it>
2023-05-08 11:56:31 +0200

    fix

--- .github/workflows/toolchains/toolchain-arm64.cmake
+++ .github/workflows/toolchains/toolchain-arm64.cmake
@@ -9,26 +9,26 @@ set(CMAKE_CXX_COMPILER_TARGET ${TARGET_TRIPLE})
 set(CMAKE_CXX_COMPILER /usr/bin/clang++-10)
 set(CMAKE_ASM_COMPILER_TARGET ${TARGET_TRIPLE})
 set(CMAKE_ASM_COMPILER /usr/bin/clang-10)
-
-#set(CMAKE_FIND_ROOT_PATH /usr/lib/aarch64-linux-gnu)
-#set(CMAKE_SYSROOT /usr/lib/aarch64-linux-gnu)
-
 set(CMAKE_AR /usr/bin/llvm-ar-10)
 
+#set(CMAKE_FIND_ROOT_PATH /usr/lib/aarch64-linux-gnu)
+set(CMAKE_SYSROOT /usr/lib/aarch64-linux-gnu)
+
+
 # Set various compiler flags
-#set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld -v")
-#set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld -v")
-#set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld -v")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld" "-v")
+set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld" "-v")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld" "-v")
 set(CMAKE_CXX_FLAGS_INIT "-stdlib=libc++")
 
 
 # Use our definitions for compiler tools
-#SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
 # Search for libraries and headers in the target directories only
-#SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-#SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-#SET(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 # This must be set or compiler checks fail when linking
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
