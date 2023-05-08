baf9d2d778c3d3c881754c57dccf3fb77a40edaf
Andrea Cavalli <andrea@cavallium.it>
2023-05-08 11:16:21 +0200

    fix

--- .github/workflows/toolchains/toolchain-arm64.cmake
+++ .github/workflows/toolchains/toolchain-arm64.cmake
@@ -11,8 +11,10 @@ set(CMAKE_CXX_COMPILER_TARGET ${triple})
 set(CMAKE_AR /usr/bin/llvm-ar-10)
 
 # Set various compiler flags
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fuse-ld=lld")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fuse-ld=lld -stdlib=libc++")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
 
 
 # Use our definitions for compiler tools
