cd6c20969e2bdde80090c4ae8a0f6f4da5796290
Andrea Cavalli <andrea@cavallium.it>
2023-05-11 18:03:50 +0200

    fix dependencies

--- .github/workflows/scripts/install_deps_linux_arm64_ssl3.sh
+++ .github/workflows/scripts/install_deps_linux_arm64_ssl3.sh
@@ -13,20 +13,20 @@ echo "deb [arch=arm64,armhf,ppc64el,riscv64,s390x] http://ports.ubuntu.com/ $(ls
 apt-get --assume-yes update
 
 # Install and bypass a bug in the cross-platform libc++ packages
-apt-get --assume-yes autoremove libc++-14-dev libc++abi-14-dev libc++1-14 libc++abi1-14 libunwind-14-dev || true
-apt-get --assume-yes -o Dpkg::Options::="--force-overwrite" install libc++-14-dev:arm64 libc++abi-14-dev:arm64 libunwind-14-dev:arm64
+apt-get --assume-yes autoremove libc++-14-dev libc++abi-14-dev libc++1-14 libc++abi1-14 libunwind-14-dev libunwind-14 || true
+apt-get --assume-yes -o Dpkg::Options::="--force-overwrite" install --reinstall libc++-14-dev:arm64 libc++abi-14-dev:arm64 libunwind-14-dev:arm64
 rm -r \
   /usr/lib/aarch64-linux-gnu/libc++abi.so \
   /usr/lib/aarch64-linux-gnu/libc++abi.so.1.0 \
   /usr/lib/aarch64-linux-gnu/libc++.so.1.0 \
-  /usr/lib/aarch64-linux-gnu/libunwind.so.1.0
+  /usr/lib/aarch64-linux-gnu/libunwind.so.1.0 || true
 mv -t /usr/lib/aarch64-linux-gnu/ \
   /usr/lib/llvm-14/lib/libc++abi.so \
   /usr/lib/llvm-14/lib/libc++abi.so.1.0 \
   /usr/lib/llvm-14/lib/libc++.so \
   /usr/lib/llvm-14/lib/libc++.so.1.0 \
-  /usr/lib/llvm-14/lib/libunwind.so.1.0
-apt-get --assume-yes -o Dpkg::Options::="--force-overwrite" install libc++-14-dev libc++abi-14-dev libunwind-14-dev
+  /usr/lib/llvm-14/lib/libunwind.so.1.0 || true
+apt-get --assume-yes -o Dpkg::Options::="--force-overwrite" install --reinstall libc++-14-dev libc++abi-14-dev libunwind-14-dev
 # End libc++ packages bugfix
 
 # Add arm64 common files
