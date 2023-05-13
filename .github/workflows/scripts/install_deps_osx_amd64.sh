d95cfd7fedb718068e62797015d674c79f20a01c
Andrea Cavalli <andrea@cavallium.it>
2023-05-13 14:57:32 +0200

    Add windows 2019, macos 12

---
+++ .github/workflows/scripts/install_deps_osx_amd64.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -ex
+brew update
+brew install gperf cmake openssl coreutils
+brew install openjdk
+brew install freetype gd libtiff
---
+++ .github/workflows/scripts/install_deps_windows_amd64.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -ex
+# Windows 2019
+choco install base64
+choco install gperf
+choco install strawberryperl
