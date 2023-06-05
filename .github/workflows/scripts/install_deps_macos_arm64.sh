d85b961d283192dc2673dc7cb42e186c0d02b46f
Andrea Cavalli <andrea@cavallium.it>
2023-06-05 22:47:21 +0200

    Fix macOS build

--- .github/workflows/scripts/install_deps_macos_arm64.sh
+++ .github/workflows/scripts/install_deps_macos_arm64.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -ex
 brew update
-brew install --force gperf openssl coreutils
+brew install --force gperf
+brew install --force coreutils
 brew install --force openjdk
-brew install --force freetype gd libtiff
+brew install --force freetype
+brew install --force gd
+brew install --force libtiff
