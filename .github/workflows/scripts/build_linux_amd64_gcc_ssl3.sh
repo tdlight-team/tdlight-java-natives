69bff16d119410f5085e890338ee152a7ffef53e
Andrea Cavalli <andrea@cavallium.it>
2023-06-05 16:25:14 +0200

    Add gcc variant

--- .github/workflows/scripts/build_linux_amd64_gcc_ssl3.sh
+++ .github/workflows/scripts/build_linux_amd64_gcc_ssl3.sh
@@ -6,9 +6,6 @@ REVISION="${REVISION:-1.0.0.0-SNAPSHOT}"
 rm -rf implementations/tdlight/td_tools_build implementations/tdlight/build api/target-legacy api/target api/.ci-friendly-pom.xml implementations/tdlight/td/generate/auto natives/src/main/java/it/tdlight/jni natives/build natives/tdjni_bin natives/tdjni_docs
 mkdir -p implementations/tdlight/build  implementations/tdlight/build/td_bin/bin implementations/tdlight/td_tools_build/java/it/tdlight/jni api/src/main/java-legacy/it/tdlight/jni api/src/main/java-sealed/it/tdlight/jni natives/src/main/java/it/tdlight/jni natives/build natives/tdjni_bin natives/tdjni_docs
 
-# Use c++11
-export CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=c++14"
-
 # ccache
 CCACHE=$(which sccache || true)
 if [[ -x "$CCACHE" ]]; then
@@ -46,7 +43,7 @@ cd ../../../
 export CMAKE_C_FLAGS="${CMAKE_C_FLAGS}"
 export CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -fno-omit-frame-pointer -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti"
 export CMAKE_SHARED_LINKER_FLAGS="${CMAKE_SHARED_LINKER_FLAGS} -Wl,--gc-sections -Wl,--exclude-libs,ALL"
-export CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -flto=thin -O3"
+export CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -flto=n -Os"
 
 # Build tdlib
 cd implementations/tdlight/build
