9f2a2774d38b1aff723e2aa176b7374738fcc3f7
Andrea Cavalli <andrea@cavallium.it>
2025-09-02 19:19:34 +0200

    Fix sccache

--- .docker/install-sccache.sh
+++ .docker/install-sccache.sh
@@ -2,11 +2,11 @@
 if [ ! -f /opt/sccache/sccache ]; then
   apt-get --assume-yes update
   apt-get --assume-yes install wget
-  wget https://github.com/mozilla/sccache/releases/download/v0.5.4/sccache-v0.5.4-x86_64-unknown-linux-musl.tar.gz
-  tar xzf sccache-v0.5.4-x86_64-unknown-linux-musl.tar.gz
-  rm sccache-v0.5.4-x86_64-unknown-linux-musl.tar.gz
+  wget https://github.com/mozilla/sccache/releases/download/v0.10.0/sccache-dist-v0.10.0-x86_64-unknown-linux-musl.tar.gz
+  tar xzf sccache-dist-v0.10.0-x86_64-unknown-linux-musl.tar.gz
+  rm sccache-dist-v0.10.0-x86_64-unknown-linux-musl.tar.gz
   mkdir -p /opt/sccache/
-  mv sccache-v0.5.4-x86_64-unknown-linux-musl/sccache /opt/sccache/sccache
-  rm -r sccache-v0.5.4-x86_64-unknown-linux-musl
+  mv sccache-dist-v0.10.0-x86_64-unknown-linux-musl/sccache /opt/sccache/sccache
+  rm -r sccache-dist-v0.10.0-x86_64-unknown-linux-musl
   chmod +x /opt/sccache/sccache
 fi
